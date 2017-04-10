9eb3b26b5f46204b3a161ebb618b52be9f728391
Jorian Wielink <jorian@outlook.com>
2017-04-10 22:31:34 +0200

    Some admin section enhancements

--- bookshare/models.py
+++ bookshare/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Author(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
-    email = models.EmailField(verbose_name='e-mail')
+    email = models.EmailField(blank=True, verbose_name='e-mail')
 
     def __str__(self):
         return u'%s %s' % (self.first_name, self.last_name)
@@ -13,7 +13,10 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=128)
     authors = models.ManyToManyField(Author)
-    publication_date = models.DateField()
+    publication_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ['title']
