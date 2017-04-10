b5eefe774afd032b9c5239659570d3b714b16d1f
Jorian Wielink <jorian@outlook.com>
2017-04-10 20:53:09 +0200

    tnb3

---
+++ bookshare/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^time/$', views.current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', ),
+]
