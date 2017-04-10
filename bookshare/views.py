297765968878dec530b2a6453c2ab616eef0694e
Jorian Wielink <jorian@outlook.com>
2017-04-10 20:46:26 +0200

    tnb3

--- bookshare/views.py
+++ bookshare/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello there!")
+
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "It is now %s." % now
+    return HttpResponse(html)
