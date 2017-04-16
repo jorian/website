6b05ec317a2562c9a2e24cb76d5642cbe820976f
n41r0j <jorian@outlook.com>
2017-04-16 13:38:23 +0200

    ch forms

--- bookshare/urls.py
+++ bookshare/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^time/$', views.current_datetime),
     url(r'^time/plus/(\d{1,2})/$', views.hours_ahead),
     url(r'^search/$', views.search),
+    url(r'^metadata/$', views.meta_data)
 ]
--- bookshare/views.py
+++ bookshare/views.py
@@ -23,13 +23,23 @@ def hours_ahead(request, offset):
 
 
 def search(request):
-    error = False
+    errors = []
     if 'q' in request.GET:
         q = request.GET['q']
         if not q:
-            error = True
+            errors.append('Please submit a search term')
+        elif len(q) > 20:
+            errors.append('Please enter max. 20 characters')
         else:
             books = Book.objects.filter(title__icontains=q)
             return render(request, 'search_result.html',
                           {'books': books, 'query': q})
-    return render(request, 'search_form.html', {'error': error})
+    return render(request, 'search_form.html', {'errors': errors})
+
+
+def meta_data(request):
+    values = request.META.items()
+    html = []
+    for k, v in values:
+        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
+    return HttpResponse('<table>%s</table>' % '\n'.join(html))
